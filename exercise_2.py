86dafc9b23f73078d68b9554552af7c49142349b
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 00:24:34 +0800

    description

---
+++ exercise_2.py
@@ -0,0 +1,8 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 2: 
+# Print the sum of the current number and the previous number
+# Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+
